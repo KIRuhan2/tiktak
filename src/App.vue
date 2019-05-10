79bba1411a9c0a332a24fe9a9466571a8304eab1
Kirill <kirill77785@gmail.com>
2019-05-10 18:24:39 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,243 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div @click="settingsClick()" :class="['settings', settings.status ? 'settings--active': '']">
+      <div class="settings__icon"
+        :style="{
+          transform:`translateX(${settings.rotate/1.5}px) rotate(${settings.rotate}deg`,
+        }"
+      ></div>
+      <div class="settings__icon--x"
+        :style="{
+          transform:`translate(${settings.rotate/1.5}px)`
+        }"
+      ></div>
     </div>
-    <router-view/>
+    <div :class="['sidepanel', settings.status ? 'sidepanel--active' : '']">
+      <div class="sidepanel__option">
+          <div class="sidepanel__option__item">Cell Size <span class ="default" @click="fieldSettings.cellSize = 100">Default</span></div>
+          <div class="sidepanel__option__row">
+            <input class = "cellSizeValue" v-model="fieldSettings.cellSize" type="range" min="20" max="200" value="100">
+            <div class="sidepanel__option__row__value">{{ fieldSettings.cellSize }}</div>
+          </div>
+      </div>
+    </div>
+    <div class="highText">
+      <div class="winStatus">
+        <span v-if="highText">{{highText}}</span> <span :class="winnerClass !== '' ? (winnerClass+' cell') :'' "></span>
+      </div>
+        <div v-if="restartButton" @click="resetGame()" class="restart">Reset</div>
+    </div>
+    <field ref = "field" :options="fieldSettings" @GameEnd="gameEnd" />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import field from '@/components/field.vue'
+
+export default {
+  name: 'app',
+  components: {
+    field
+  },
+  data: function () {
+    return {
+      settings: {
+        status: false,
+        rotate: 0
+      },
+      fieldSettings: {
+        cellSize: 100,
+        winRow: 3,
+        matrixSize: 3,
+        disabled: false
+      },
+      highText: '',
+      winnerClass: '',
+      restartButton: false
+    }
+  },
+
+  methods: {
+    settingsClick () {
+      this.settings.rotate += this.settings.status ? -90 : 90
+      this.settings.status = !this.settings.status
+    },
+    resetGame () {
+      this.highText = ''
+      this.winnerClass = ''
+      this.fieldSettings.disabled = false
+      this.restartButton = false
+      this.$refs.field.restartGame()
+    },
+    gameEnd (winner) {
+      this.highText = winner === false ? 'Ничья!' : 'Победа!'
+      this.winnerClass = ['', 'x', 'o'][winner || 0]
+      this.fieldSettings.disabled = true
+      this.restartButton = true
+    }
+  }
 }
-#nav {
-  padding: 30px;
+</script>
+
+<style scoped>
+
+#app{
+  font-family: sans-serif;
+  font-size: 45px;
+}
+ /*Settings*/
+.settings{
+  display: flex;
+  z-index: 2;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  left: 10px;
+  top: 10px;
+  width: 60px;
+  overflow: hidden;
+  height:60px;
+  background: linear-gradient(#fff, #eee);
+  box-shadow: 0 0 0 1px #000;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: .2s;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.settings--active{
+  background:#fff;
+  box-shadow: none;
+
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.settings__icon{
+  transition: .4s;
+  background-image: url("~@/assets/icons/settings.svg");
+  background-position: center;
+  position: absolute;
+  width: 49px;
+  height: 49px;
 }
+
+.settings__icon--x{
+  transition: .4s;
+  background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48' version='1.1' width='48px' height='48px'%3E%3Cg id='surface1'%3E%3Cpath style=' fill:%23000;' d='M 36.019531 8.445313 L 39.558594 11.980469 L 11.980469 39.554688 L 8.445313 36.019531 Z '/%3E%3Cpath style=' fill:%23000;' d='M 39.554688 36.023438 L 36.019531 39.558594 L 8.445313 11.976563 L 11.980469 8.441406 Z '/%3E%3C/g%3E%3C/svg%3E%0A");
+  fill:#000;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%, 100%, auto;
+  position: absolute;
+  left:  -55px;
+  width: 49px;
+  height: 49px;
+}
+
+.highText{
+  height: 140px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+}
+
+.winStatus{
+  height: 100px;
+  align-items: center;
+  display: flex;
+}
+
+.field{
+  margin-top: 20px;
+}
+
+.sidepanel{
+  z-index: 1;
+  will-change: transform;
+  position: fixed;
+  transform: translateX(-100%);
+  padding-top: 100px;
+  top: 0;
+  padding-right: 10px;
+  transition: .3s;
+  height: 100vh;
+  width: 340px;
+  background: #fff;
+}
+
+.sidepanel--active{
+  box-shadow: 18px 0px 20px 2px rgba(191,191,191,1);
+  transform: translateX(0);
+}
+
+.restart{
+  width: 4em;
+  height: 2em;
+  margin: 0 auto;
+  font-size: 20px;
+  border: 2px solid #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.restart:hover{
+  cursor: pointer;
+  background-color: #000;
+  color: #fff;
+}
+
+.cellSizeValue:focus{
+  outline: none
+}
+.cellSizeValue:focus::-webkit-slider-thumb{
+  background: red;
+}
+.cellSizeValue{
+  appearance: none;
+  width: 100%;
+  height: 10px;
+  background: #d3d3d3;
+  border-radius: 10px;
+  opacity: 0.7;
+  transition: opacity .2s;
+}
+
+.cellSizeValue::-webkit-slider-thumb{
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  background: #000;
+  border-radius: 50%;
+}
+
+.cellSizeValue::-webkit-range-thumb {
+  width: 25px; /* Set a specific slider handle width */
+  height: 15px; /* Slider handle height */
+  background: #000; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
+.sidepanel__option__row{
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.sidepanel__option__row__value{
+  margin-left: 20px;
+  width: 3em;
+}
+
+.default{
+  font-size: 20px;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.cell{
+  margin-left: 15px;
+  outline: none;
+  background-color: transparent;
+}
+
 </style>
---
+++ tests/unit/field.spec.js
@@ -0,0 +1,162 @@
+import field from '@/components/field.vue'
+
+const data = field.data()
+const r = Math.round(Math.random() + 1) // random Unit (1 or 2) as ("x" or "O")
+const n = r === 1 ? 2 : 1 // reversed random Unit
+
+describe('Valid', () => {
+  it('If Data Matrix Valid', () => {
+    const flat = (arr, depth = Infinity, arr2 = []) => {
+      arr.forEach(e => {
+        typeof e === 'object' && depth ? flat(e, depth - 1, arr2) : arr2.push(e)
+      })
+      return arr2
+    }
+    expect(!!flat(data.matrix).find(x => ![0, 1, 2].includes(x))).toBe(false)
+  })
+})
+
+describe('field', () => {
+  it('Cell Status Test', () => {
+    data.matrix = [
+      [0, 1, 2],
+      [0, 1, 2], // 0: "", 1: "x", 2: "o".
+      [0, 0, 2]
+    ]
+    expect(field.methods.cellStatus.apply(data, [1, 1])).toBe('x')
+    expect(field.methods.cellStatus.apply(data, [0, 0])).toBe('')
+    expect(field.methods.cellStatus.apply(data, [1, 2])).toBe('o')
+  })
+})
+
+describe('Field Win Status', () => {
+  it('Diagonal status win', () => {
+    data.matrix = [
+      [r, 0, 0],
+      [0, r, 0],
+      [0, 0, r]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [1, 1])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [2, 2])).toBe(r)
+
+    // Test win trace detect correctly.
+    field.methods.winStatus.apply(data, [2, 2])
+    expect(data.crossLine).toEqual([[0, 0], [1, 1], [2, 2]])
+
+    data.matrix = [
+      [0, 0, r],
+      [0, r, 0],
+      [r, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 2])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [1, 1])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [2, 0])).toBe(r)
+
+    field.methods.winStatus.apply(data, [0, 2])
+    expect(data.crossLine).toEqual([[2, 0], [1, 1], [0, 2]])
+
+    data.matrix = [
+      [0, 0, r],
+      [0, 0, 0],
+      [r, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 2])).toBe(false)
+
+    data.matrix = [
+      [n, r, r],
+      [0, r, 0],
+      [0, 0, r]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0])).toBe(false)
+
+    data.matrix = [
+      [r, 0, 0],
+      [0, r, 0],
+      [0, 0, r]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0, 4])).toBe(false)
+
+    data.matrix = [
+      [r, 0, 0, 0],
+      [0, r, 0, 0],
+      [0, 0, r, 0],
+      [0, 0, 0, r]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0, 4])).toBe(r)
+
+    field.methods.winStatus.apply(data, [0, 0, 4])
+    expect(data.crossLine).toEqual([[0, 0], [1, 1], [2, 2], [3, 3]])
+
+    data.matrix = [
+      [0, 0, n, r],
+      [0, n, r, 0],
+      [n, r, 0, 0],
+      [r, 0, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 3, 4])).toBe(r)
+    field.methods.winStatus.apply(data, [2, 1, 4])
+    expect(data.crossLine).toEqual([[ 3, 0 ], [ 2, 1 ], [ 1, 2 ], [ 0, 3 ]])
+  })
+
+  it('Row Status win', () => {
+    data.matrix = [
+      [r, r, r],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [0, 1])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [0, 2])).toBe(r)
+
+    expect(field.methods.winStatus.apply(data, [1, 1])).toBe(false)
+
+    data.matrix = [
+      [r, r, r],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [[0, 0, 4]])).toBe(false)
+
+    data.matrix = [
+      [r, r, r, r],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0, 4])).toBe(r)
+
+    field.methods.winStatus.apply(data, [0, 0, 4])
+    expect(data.crossLine).toEqual([[ 0, 0 ], [ 0, 1 ], [ 0, 2 ], [ 0, 3 ]])
+  })
+
+  it('Column Status win', () => {
+    data.matrix = [
+      [r, 0, 0],
+      [r, 0, 0],
+      [r, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [1, 0])).toBe(r)
+    expect(field.methods.winStatus.apply(data, [2, 0])).toBe(r)
+
+    data.matrix = [
+      [r, 0, 0],
+      [r, 0, 0],
+      [r, 0, 0],
+      [r, 0, 0]
+    ]
+    expect(field.methods.winStatus.apply(data, [0, 0, 4])).toBe(r)
+
+    data.matrix = [
+      [r, 0, 0],
+      [n, 0, 0],
+      [r, 0, 0],
+      [r, 0, 0],
+      [n, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]
+
+    expect(field.methods.winStatus.apply(data, [2, 0])).toBe(false)
+  })
+})
